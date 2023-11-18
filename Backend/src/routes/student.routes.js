4bc81e675d18ba18c9f0fd38bade6f76fb2cbdf4
Wibters <143566599+Wibters2019@users.noreply.github.com>
2023-11-18 15:48:00 -0500

    Update student.routes.js

---
+++ Backend/src/routes/student.routes.js
@@ -0,0 +1,22 @@
+const Router = require("express");
+const{
+  getRegisterStudent,
+  getUpdateStudent,
+  getVisualiseStudent,
+} = require("../controllers/student.controllers.js");
+
+const studentRouter = Router();
+
+studentRouter.post(
+  "/api/student/getRegisterStudent",
+  getRegisterStudent
+);
+studentRouter.put(
+  "/api/student/getUpdateStudent",
+  getUpdateStudent
+);
+studentRouter.get(
+  "/api/student/getVisualiseStudent",
+  getVisualiseStudent
+);
+module.exports = studentRouter;
