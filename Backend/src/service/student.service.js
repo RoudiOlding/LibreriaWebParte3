774b317f3b05239181cdb0778af2b24c083c1149
Wibters <143566599+Wibters2019@users.noreply.github.com>
2023-11-18 14:59:55 -0500

    Actualizado x2

--- Backend/src/service/student.service.js
+++ Backend/src/service/student.service.js
@@ -48,26 +48,4 @@ const getVisualiseStudentService = async (StudentId) =>{
         throw Error("Error while visualise Student: " + e);
     }
 };
-const getFavoriteStudentService = async (StudentId, FavoriteId) =>{
-    
-};
 
-
-//Todo lo que hace el alumno
-
-/*
-    Acciones & Atributos:
-        1. Buscar libros
-            a. INgresas palabras clave o filtrados, para obtener resultados
-        2. Reservar libros:
-            a. Selecciona el libro que tiene prestado titutlo isbn autor editor
-            b. Clickea en devolver y el libro cambia su atributo "prestado: true" a "prestado: false"
-        3. Crear lista de libros favoritos:
-            a. Puede agregar libros a la lista
-            b. Puede crear nuevas listas (con nombre personalizado)
-        4. Modificar su cuenta:
-            a. Nombres, apellido, nro de documento, correo, contraseña
-        5. Resgirar cuenta
-            a. Nombres, Apellidos, Tipo de documento, nro de documento, tipo de documento, correo, contraseña
-        
-*/
