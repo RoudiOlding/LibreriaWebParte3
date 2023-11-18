08123bd7e50727bb78291dc69b53fb71c30bee73
Wibters <143566599+Wibters2019@users.noreply.github.com>
2023-11-18 15:07:11 -0500

    Update admin

--- Backend/src/service/administrator.service.js
+++ Backend/src/service/administrator.service.js
@@ -1,14 +1,51 @@
 const { Models } = require("../db.js");
 
-//Todo lo que hace el administrador
-
-/*
-    Acciones & Atributos:
-        1. Agregar libros
-            a. Todos los atributos de un libro puede agregar
-        2. Buscar libros:
-            a. Por nombre unicamente
-        3. Modificar su cuenta:
-            a. Nombres, Tipo de documento, apellido, nro de documento, correo, contraseña, idioma, prefijo, color
-*/
+const getRegisterAdministratorService = async (body) =>{
+    try{
+        const administrator = await Models.Administrator.create({
+            name:body.name,
+            lastName:body.lastName,
+            email:body.email,
+            password:body.password,
+            identityDoc:body.identityDoc,
+            photo:body.photo,
+        });
+        return student;
+    }catch(e){
+        throw Error("Error while creating Administrator: " + e);
+    }
+}
+const getUpdateAdministratorService = async (body) =>{
+    try{
+        const administrator = await Models.Administrator.findOne({
+            where:{
+                id: AdministratorId,
+            },
+        });
+        await patiend.update({
+            name:body.name,
+            lastName:body.lastName,
+            email:body.email,
+            password:body.password,
+            identityDoc:body.identityDoc,
+            photo:body.photo
+        });
+       return administrator;
+    }catch(e){
+        throw Error("Error while update Administrator: " + e);
+    }
+}
+const getVisualiseAdministratorService = async (AdministratorId) =>{
+    try{
+        const administrator = await Models.Administrator.findOne({
+            attributes: ["name","lastName","email","password","identityDoc","photo"]
+            where:{
+                id:AdministratorId,
+            },
+        });
+      return administrator;
+    }catch(e){
+        throw Error("Error while visualise Administrator: " + e);
+    }
+};
 
