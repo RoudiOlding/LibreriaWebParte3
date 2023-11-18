748a773e7729e4c22f28cd1822743f12a28f9c94
Wibters <143566599+Wibters2019@users.noreply.github.com>
2023-11-18 15:26:15 -0500

    Update controller

---
+++ Backend/src/controllers/student.controllers.js
@@ -0,0 +1,47 @@
+const{
+  getRegisterStudentService,
+  getUpdateStudentService,
+  getVisualiseStudentService,
+} = require("../service/student.service.js");
+const getRegisterStudent = async (req,res) =>{
+  try{
+    const result = await getRegisterStudentService(req.body);
+    return res.status(200).json({
+      status: 200,
+      result: result,
+      message: "Succesfully Student Register",
+    });
+  }; catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
+const getUpdateStudent = async (req,res) =>{
+  try{
+    const result = await getUpdateStudentService(req.body);
+    return res.status(200).json({
+      status: 200,
+      result: result,
+      message: "Succesfully Student Update",
+    });
+  }; catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
+const getVisualiseStudent = async (req, res) => {
+  try {
+    const result = await getVisualiseStudentService(req.user.id) || [];
+
+    return res.status(200).json({
+      status: 200,
+      result: result,
+      message: "Succesfully Student Returned",
+    });
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
+module.exports ={
+  getRegisterStudent,
+  getUpdateStudent,
+  getVisualiseStudent,
+};
